8b24dcaa22fd13ac31eba94a3c212e8490a85524
Paul Hottell <paul.hottell@gmail.com>
2017-01-02 14:12:25 -0800

    Finished PokeDex app

---
+++ PokeDex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PokeDex
+//
+//  Created by Paul Hottell on 1/1/17.
+//  Copyright © 2017 HSCS. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon"
+
+typealias DownloadComplete = () -> ()
---
+++ PokeDex/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by Paul Hottell on 12/31/16.
+//  Copyright © 2016 HSCS. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
